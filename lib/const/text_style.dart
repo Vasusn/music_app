f1db7c0f48f4e8236d86194f073bed3f11246d49
Vasusn <vasunageshri@gmail.com>
2023-08-07 18:35:57 +0530

    commit

---
+++ lib/const/text_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'const.dart';
+
+ourStyle({double? size = 14, color = whiteColor}) {
+  return TextStyle(
+    fontSize: size,
+    color: color,
+  );
+}
